4bf92a7e7088d3fe5e584bb61b58ff2a9d784717
Rodrigo A. Mora Azócar <brazuk25@gmail.com>
2023-02-19 19:11:29 -0300

    Alterações: pom.xml -  dependencia mysql application.properties - modifiquei os parametros

--- api/src/main/java/br/com/projeto/api/controle/Controle.java
+++ api/src/main/java/br/com/projeto/api/controle/Controle.java
@@ -1,5 +1,14 @@
 package br.com.projeto.api.controle;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controle {
 
+	@GetMapping("/")
+	public String teste() {
+		return "Hello World!";
+	}
+	
 }
