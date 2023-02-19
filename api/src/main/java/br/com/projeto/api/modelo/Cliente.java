fece0369d50210f7a1a950823c18c8897483c3c3
Rodrigo A. Mora Azócar <brazuk25@gmail.com>
2023-02-19 19:37:40 -0300

    Criação da classe Cliente usando Lombok- Foi verificada a tabela clientes no Mysql

--- api/src/main/java/br/com/projeto/api/modelo/Cliente.java
+++ api/src/main/java/br/com/projeto/api/modelo/Cliente.java
@@ -1,5 +1,28 @@
 package br.com.projeto.api.modelo;
 
-public class Cliente {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Table(name="clientes")
+@Getter
+@Setter
+
+public class Cliente {
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private long codigo;
+	private String nome;
+	private int idade;
+	private String cidade;
+	
+	
+	
+	
+	
 }
